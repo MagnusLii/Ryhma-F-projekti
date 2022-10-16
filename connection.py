75f535713a7778aab3871ad90c1ea5354f91e808
Mage <magnus.liivamagi@gmail.com>
2022-10-16 23:22:28 +0300

    reworking random_goal_gen func.

---
+++ connection.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+sqlconnect = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="5579",
+    autocommit=True
+)
