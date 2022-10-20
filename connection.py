160620b16c4c226d6460fe36ba3194061dfa7c0d
Mage <magnus.liivamagi@gmail.com>
2022-10-20 16:25:38 +0300

    Setting up databaseupdate.sql code for easy updates to DB in future.

--- connection.py
+++ connection.py
@@ -1,3 +1,7 @@
+"""Set your own DB connection settings here
+DO NOT PUSH TO GIT UNDER ANY CIRCUMSTANCES!!!!
+I will commit stop existence upon you if you do!"""
+
 import mysql.connector
 
 sqlconnect = mysql.connector.connect(
