ee33c830446d8846cb94be723a45243fdabc5669
Mage <magnus.liivamagi@gmail.com>
2022-10-20 19:35:47 +0300

    Testing kmfromgoal func.

--- functions.py
+++ functions.py
@@ -826,9 +826,12 @@ def kmfromgoal(currentloc=currentplayer, currentgoal=currentgoalid()):
                      FROM airport, game
                      WHERE game.id = {currentloc}
                      ;''')
-    goalid = cursor_fetchall(f'''SELECT airport.latitude_deg, airport.longitude_deg
-                             FROM airport
-                             WHERE airport.id = {currentgoal}''')
-    print(currentcoords)
-    print(goalid)
-    return str(geopy.distance.geodesic(currentcoords, goalid))
+    airportid = cursor_fetchall(f'''SELECT goal.airportid
+                             FROM goal_reached, goal
+                             WHERE goal_reached.goal_id = {currentgoal}
+                             ;''')
+    print(str(geopy.distance.geodesic(currentcoords, cursor_fetchall(f'''SELECT airport.latitude_deg,
+                                                                     airport.longitude_deg
+                                                                     FROM airport
+                                                                     WHERE airport.id = {airportid}
+                                                                     ;'''))))
